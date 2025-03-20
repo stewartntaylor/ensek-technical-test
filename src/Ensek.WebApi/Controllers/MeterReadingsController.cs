14e222ecf7516c8eb1406443330acf9ea5d292e0
Stewart Taylor <stewart.taylor@outlook.com>
2025-03-20 08:21:16 +0000

    Additional error handling

--- src/Ensek.WebApi/Controllers/MeterReadingsController.cs
+++ src/Ensek.WebApi/Controllers/MeterReadingsController.cs
@@ -2,6 +2,7 @@
 using Ensek.WebApi.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Ensek.WebApi.Controllers
 {
@@ -18,18 +19,27 @@ namespace Ensek.WebApi.Controllers
 		[HttpPost("/meter-reading-uploads")]
 		public async Task<ActionResult<MeterReadingsUploadResponse>> UploadMeterReadings([FromForm] MeterReadingsUploadRequest request)
 		{
-			if (request?.File == null || request.File.Length == 0)
+			try
 			{
-				return BadRequest("No file uploaded");
+				if (request?.File == null || request.File.Length == 0)
+				{
+					return BadRequest("No file uploaded");
+				}
+
+				var tempFile = await StoreTempFile(request.File);
+
+				var processingResult = await _sender.Send(new SubmitMeterReadingsCommand(tempFile));
+
+				DeleteTempFile(tempFile);
+
+				return new MeterReadingsUploadResponse(processingResult.RecordsProcessed, processingResult.Errors);
 			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Error processing file upload");
 
-			var tempFile = await StoreTempFile(request.File);
-
-			var processingResult = await _sender.Send(new SubmitMeterReadingsCommand(tempFile));
-
-			DeleteTempFile(tempFile);
-
-			return new MeterReadingsUploadResponse(processingResult.RecordsProcessed, processingResult.Errors);
+				return Problem("Error processing meter readings file upload", statusCode: (int)HttpStatusCode.InternalServerError);
+			}
 		}
 
 		private async Task<string> StoreTempFile(IFormFile formFile)
